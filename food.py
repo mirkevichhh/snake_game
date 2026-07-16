4536d849cbdad2d5f0f5550fae5c954b0646975c
Illia Mirkevych <illia.mirkevych@student.tuke.sk>
2026-07-16 17:14:20 +0300

    Update food.py

--- food.py
+++ food.py
@@ -6,21 +6,20 @@ class Food:
         self.position = self.generate_random_position(snake_body)
     def generate_random_position(self,snake_body):
         free_points = []
-        for x in range(settings.GRID_WIDTH):
-            for y in range(settings.GRID_HEIGHT):
+        for x in range(1,settings.GRID_WIDTH-1):
+            for y in range(2,settings.GRID_HEIGHT-1):
                 if (x,y) not in snake_body:
                     free_points.append((x,y))
         if not free_points:
             return None
         return random.choice(free_points)
 
-    def draw(self,surface):
+    def draw(self,surface, apple_img):
+        
         if self.position is None:
             return
-        pixel_x = self.position[0] * settings.CELL_SIZE + settings.CELL_SIZE//2
-        pixel_y = self.position[1] * settings.CELL_SIZE + settings.CELL_SIZE//2
-        
-        food_radius = settings.CELL_SIZE//6
-
-        pygame.draw.circle(surface, (255, 0, 0), (pixel_x,pixel_y), food_radius)
+        pixel_x = self.position[0] * settings.CELL_SIZE 
+        pixel_y = self.position[1] * settings.CELL_SIZE 
+         
+        surface.blit(apple_img,(pixel_x,pixel_y))
 
